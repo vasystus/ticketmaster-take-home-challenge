4b5b17a50de3aafbd88d62ec82c209a9c6adb6f5
artgalla8 <artgalla8@gmail.com>
2022-03-13 00:20:09 +0000

    pop up done

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 function openContactForm() {
-    document.getElementById("open").style.display = "block";
+  document.getElementById("open").style.display = "block";
+}
+
+function closeContactForm() {
+  document.getElementById("open").style.display = "none";
 }
