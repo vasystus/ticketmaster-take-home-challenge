12e194a49dba15ea5d71f0bc4f708f36b85020bb
artgalla8 <artgalla8@gmail.com>
2022-03-13 20:52:42 +0000

    alert added

--- script.js
+++ script.js
@@ -5,3 +5,9 @@ function openContactForm() {
 function closeContactForm() {
   document.getElementById("open").style.display = "none";
 }
+
+let form = document.querySelector("form");
+form.addEventListener("submit",(e) => {
+e.preventDefault();
+alert("Thank you for being awesome! Form submitted.");
+});
